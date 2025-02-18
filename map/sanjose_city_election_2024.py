0bc10e8ee47d2eff02dbb4a233230a54104503fa
J. Chen <140699748+mzwsInJjq@users.noreply.github.com>
2025-02-18 18:04:08 +0800

    Update sanjose_city_election_2024.py

--- map/sanjose_city_election_2024.py
+++ map/sanjose_city_election_2024.py
@@ -20,7 +20,7 @@ df = pd.read_excel(BytesIO(response.content))
 # Convert the IDs to string and strip any whitespace
 precinct_ids = df['Precinct'].astype(str).str.strip().tolist()
 
-# Modify each precinct ID by prepending '06085-00'
+# Modify each precinct ID by prepending '06085-000'
 precincts = [f"06085-000{pid}" for pid in precinct_ids]
 
 # Output the resulting list
